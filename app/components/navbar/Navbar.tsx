59ef6a45df6d82e662389cf1ae3256148092004e
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-26 21:53:36 +0100

    logo.jsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Container from '../Container'
+/** @format */
+
+import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
 
 function Navbar() {
-  return (
-    <div className='fixed w-full bg-white shadow-sm'>
-        <div className="py-4 border-b-[1px]">
-            <Container>
-                ggg
-            </Container>
-        </div>
-      Navbar
-    </div>
-  )
+	return (
+		<div className='fixed w-full bg-white shadow-sm'>
+			<div className='py-4 border-b-[1px]'>
+				<Container>
+					<div className='flex flex-row items-center justify-between gap-3 md:gap-0 '>
+                       <Logo/> 
+                    </div>
+				</Container>
+			</div>
+			Navbar
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;

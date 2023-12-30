62a62c0024766c8489e8cbd47981b72052a0ab0c
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-30 22:07:39 +0100

    updating the next.config.js to be able to get the url of github profile image

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -34,7 +34,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 				className='p-4 md:py-1 md:px-1 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
 				<AiOutlineMenu />
 				<div className='hidden md:block'>
-					<Avatar />
+					<Avatar src ={currentUser?.image} />
 				</div>
 			</div>
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+        appDir:true
+    },
+    images:{
+        domains:[
+            "avatars.githubusercontent.com",
+        ]
+    }
+}
 
 module.exports = nextConfig

7d1f05afdd5fbd592dc82cf65012284c4abb43c6
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-04 01:13:37 +0100

    getting started with the location

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -21,33 +21,36 @@ const RentModal = () => {
 
 	const [step, setStep] = useState(STEPS.CATEGORY);
 
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        watch,
-        formState:{
-            errors,
-        },
-        reset       
-    } = useForm<FieldValues>({
-        defaultValues:{
-            category:'',
-            location:null,
-            guestCount:1,
-            roomCount:1,
-            bathroomCount:1,
-            imageSrc:'',
-            price:1,
-            title:'',
-            description:''
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		watch,
+		formState: { errors },
+		reset,
+	} = useForm<FieldValues>({
+		defaultValues: {
+			category: "",
+			location: null,
+			guestCount: 1,
+			roomCount: 1,
+			bathroomCount: 1,
+			imageSrc: "",
+			price: 1,
+			title: "",
+			description: "",
+		},
+	});
 
-        }
-    })
+	const category = watch("category");
 
-    const category = watch('category');
-
-    const setCustomValue = (id:string,value:any);
+	const setCustomValue = (id: string, value: any) => {
+		setValue(id, value, {
+			shouldValidate: true,
+			shouldDirty: true,
+			shouldTouch: true,
+		});
+	};
 
 	const onBack = () => {
 		setStep((value) => value - 1);
@@ -85,25 +88,35 @@ const RentModal = () => {
                max-h-[50vh] 
                overflow-y-auto
                '>
-
-                {categories.map((item) => (
-                    <div key={item.label} className="col-span-1">
-                        <CategoryInput
-                          onClick={() => {}}
-                          selected={false}
-                          label={item.label}
-                          icon={item.icon}
-                        />
-                    </div>
-                ))}
-               </div>
+				{categories.map((item) => (
+					<div key={item.label} className='col-span-1'>
+						<CategoryInput
+							onClick={(category) => {
+								setCustomValue("category", category);
+							}}
+							selected={category === item.label}
+							label={item.label}
+							icon={item.icon}
+						/>
+					</div>
+				))}
+			</div>
 		</div>
 	);
+
+	if (step === STEPS.LOCATION) {
+		bodyContent = <div className="flex flex-col gap-8">
+            <Heading 
+                   title="where is your place located ? "
+                   subtitle="help guests find you"
+                />
+        </div>;
+	}
 	return (
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={rentModal.onClose}
+			onSubmit={onNext}
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}

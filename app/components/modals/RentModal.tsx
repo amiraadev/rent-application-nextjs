fe1a2bb38081bc6689682886bf2fc76a4950fc4d
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-04 01:07:14 +0100

    setCustomValue to make the category stay slected

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
 	CATEGORY = 0,
@@ -19,6 +20,34 @@ const RentModal = () => {
 	const rentModal = useRentModal();
 
 	const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState:{
+            errors,
+        },
+        reset       
+    } = useForm<FieldValues>({
+        defaultValues:{
+            category:'',
+            location:null,
+            guestCount:1,
+            roomCount:1,
+            bathroomCount:1,
+            imageSrc:'',
+            price:1,
+            title:'',
+            description:''
+
+        }
+    })
+
+    const category = watch('category');
+
+    const setCustomValue = (id:string,value:any);
 
 	const onBack = () => {
 		setStep((value) => value - 1);

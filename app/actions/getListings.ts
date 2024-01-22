c428550035c7ed47580b926c9ef4fe931ef4d9ec
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-22 17:21:44 +0100

    return the safeListing instead of the listings because there was a warning about the createdAt field in the listing

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -9,7 +9,11 @@ export default async function getListings() {
 			},
 		});
 
-        return listings;
+		const safeListings = listings.map((listing) => ({
+			...listing,
+			createdAt:listing.createdAt.toISOString()
+		}))
+        return safeListings;
 	} catch (error: any) {
 		throw new Error(error);
 	}

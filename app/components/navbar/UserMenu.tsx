9df9e8d6c73e4b1e6a198c2b2e1ddfb31724f5c6
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-26 01:17:00 +0100

    ReservationsClient.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -72,7 +72,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 							<>
 								<MenuItem onClick={() => router.push("/trips")} label='My trips' />
 								<MenuItem onClick={() => {}} label='My favorites' />
-								<MenuItem onClick={() => {}} label='My reservations' />
+								<MenuItem onClick={() => router.push("/reservations")} label='My reservations' />
 								<MenuItem onClick={() => {}} label='My properties' />
 								<MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
 								<hr />
--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+/** @format */
 
-const ReservationsClient = () => {
-  return (
-    <div>ReservationsClient</div>
-  )
+import axios from "axios";
+import React, { useCallback, useState } from "react";
+import toast from "react-hot-toast";
+import { SafeReservation, SafeUser } from "../types";
+import Heading from "../components/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/listings/ListingCard";
+import { useRouter } from "next/navigation";
+
+interface ReservationsClientProps {
+	reservation: SafeReservation[];
+	currentUser?: SafeUser | null;
 }
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+	reservation,
+	currentUser,
+}) => {
+	const router = useRouter();
+	const [deletingId, setDeletingId] = useState("");
 
-export default ReservationsClient
+	const onCancel = useCallback(
+		(id: string) => {
+			setDeletingId(id);
+			axios
+				.delete(`/api/reservations/${id}`)
+				.then(() => {
+					toast.success("Reservation canceled");
+					router.refresh();
+				})
+				.catch(() => {
+					toast.error("Something went wrong !");
+				})
+				.finally(() => {
+					setDeletingId("");
+				});
+		},
+		[router]
+	);
+	return (
+		<Container>
+			<Heading title='Reservations' subtitle='Booking on your properties' />
+		</Container>
+	);
+};
+
+export default ReservationsClient;

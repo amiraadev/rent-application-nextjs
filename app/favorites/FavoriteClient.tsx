693b514544f1b9a2f839bf92b1b7145b525343fb
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-26 14:22:24 +0100

    /favorites/page.tsx

---
+++ app/favorites/FavoriteClient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavoriteClient = () => {
+  return (
+    <div>FavoriteClient</div>
+  )
+}
+
+export default FavoriteClient
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -4,13 +4,29 @@ import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import getReservations from "@/app/actions/getReservations";
 import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoriteClient from "./FavoriteClient";
 
 const ListingPage = async () => {
-    return(
+
+    const listings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
+
+    if(listings.length === 0){
+        return(
+            <ClientOnly>
+                <EmptyState
+                  title="No favorites found"
+                  subtitle="Looks like you have no favorite listings"
+                />
+            </ClientOnly>
+        )
+    }
+
+    return (
         <ClientOnly>
-            <EmptyState
-              title="No favorites found"
-              subtitle="Looks like you have no favorite listings"
+            <FavoriteClient 
+            listings = {listings}
+            currentUser = {currentUser}         
             />
         </ClientOnly>
     )

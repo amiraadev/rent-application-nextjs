e8d4f2bbf656ad7440fbbb40064b640de8e00f17
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-27 12:04:46 +0100

    middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+export {default} from 'next-auth/middleware';
+export const config = {
+    matcher:[
+        "/trips",
+        "/reservations",
+        "/properties",
+        "/favorites",
+    ]
+}

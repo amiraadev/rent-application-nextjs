e42b86ca46f494cb3ea444f5992110aca26054ac
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-27 22:19:56 +0100

    fixing the z-index for Navbar.tsx

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,151 +1,131 @@
-'use client';
+/** @format */
+
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 import useCountries from "@/app/hooks/useCountries";
-import { 
-  SafeListing, 
-  SafeReservation, 
-  SafeUser 
-} from "@/app/types";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
-  data: SafeListing;
-  reservation?: SafeReservation;
-  onAction?: (id: string) => void;
-  disabled?: boolean;
-  actionLabel?: string;
-  actionId?: string;
-  currentUser?: SafeUser | null
-};
+	data: SafeListing;
+	reservation?: SafeReservation;
+	onAction?: (id: string) => void;
+	disabled?: boolean;
+	actionLabel?: string;
+	actionId?: string;
+	currentUser?: SafeUser | null;
+}
 
 const ListingCard: React.FC<ListingCardProps> = ({
-  data,
-  reservation,
-  onAction,
-  disabled,
-  actionLabel,
-  actionId = '',
-  currentUser,
+	data,
+	reservation,
+	onAction,
+	disabled,
+	actionLabel,
+	actionId = "",
+	currentUser,
 }) => {
-	
-	
-  const router = useRouter();
-  const { getByValue } = useCountries();
+	const router = useRouter();
+	const { getByValue } = useCountries();
 
-  const location = getByValue(data.locationValue);
+	const location = getByValue(data.locationValue);
 
-  const handleCancel = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
+	const handleCancel = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement>) => {
+			e.stopPropagation();
 
-    if (disabled) {
-      return;
-    }
+			if (disabled) {
+				return;
+			}
 
-    onAction?.(actionId)
-  }, [disabled, onAction, actionId]);
+			onAction?.(actionId);
+		},
+		[disabled, onAction, actionId]
+	);
 
-  const price = useMemo(() => {
-    if (reservation) {
-      return reservation.totalPrice;
-    }
+	const price = useMemo(() => {
+		if (reservation) {
+			return reservation.totalPrice;
+		}
 
-    return data.price;
-  }, [reservation, data.price]);
+		return data.price;
+	}, [reservation, data.price]);
 
-  const reservationDate = useMemo(() => {
-    if (!reservation) {
-      return null;
-    }
-  
-    const start = new Date(reservation.startDate);
-    const end = new Date(reservation.endDate);
+	const reservationDate = useMemo(() => {
+		if (!reservation) {
+			return null;
+		}
 
-    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
-  }, [reservation]);
+		const start = new Date(reservation.startDate);
+		const end = new Date(reservation.endDate);
 
+		return `${format(start, "PP")} - ${format(end, "PP")}`;
+	}, [reservation]);
 
-  console.log({
-		data,
-		reservation,
-		onAction,
-		disabled,
-		actionLabel,
-		actionId ,
-		currentUser,
-	  });
-
-  return (
-    <div 
-      onClick={() => router.push(`/listings/${data.id}`)} 
-      className="col-span-1 cursor-pointer group"
-    >
-      <div className="flex flex-col gap-2 w-full">
-        <div 
-          className="
+	return (
+		<div
+			onClick={() => router.push(`/listings/${data.id}`)}
+			className='col-span-1 cursor-pointer group'>
+			<div className='flex flex-col gap-2 w-full'>
+				<div
+					className='
             aspect-square 
             w-full 
-            relative 
+             relative
             overflow-hidden 
             rounded-xl
-          "
-        >
-          <Image
-            fill
-            className="
+
+          '>
+					<Image
+						fill
+						className='
               object-cover 
               h-full 
               w-full 
               group-hover:scale-110 
               transition
-            "
-            src={data.imageSrc}
-            alt="Listing"
-          />
-          <div className="
+            '
+						src={data.imageSrc}
+						alt='Listing'
+					/>
+					<div
+						className='
             absolute
             top-3
             right-3
-          ">
-            <HeartButton 
-              listingId={data.id} 
-              currentUser={currentUser}
-            />
-          </div>
-        </div>
-        <div className="font-semibold text-lg">
-          {location?.region}, {location?.label}
-        </div>
-        <div className="font-light text-neutral-500">
-          {reservationDate || data.category}
-        </div>
-        <div className="flex flex-row items-center gap-1">
-          <div className="font-semibold">
-            $ {price}
-          </div>
-          {!reservation && (
-            <div className="font-light">night</div>
-          )}
-        </div>
-        {onAction && actionLabel && (
-          <Button
-            disabled={disabled}
-            small
-            label={actionLabel} 
-            onClick={handleCancel}
-          />
-        )}
-      </div>
-    </div>
-   );
-}
- 
+          '>
+						<HeartButton listingId={data.id} currentUser={currentUser} />
+					</div>
+				</div>
+				<div className='font-semibold text-lg'>
+					{location?.region}, {location?.label}
+				</div>
+				<div className='font-light text-neutral-500'>
+					{reservationDate || data.category}
+				</div>
+				<div className='flex flex-row items-center gap-1'>
+					<div className='font-semibold'>$ {price}</div>
+					{!reservation && <div className='font-light'>night</div>}
+				</div>
+				{onAction && actionLabel && (
+					<Button
+						disabled={disabled}
+						small
+						label={actionLabel}
+						onClick={handleCancel}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
+
 export default ListingCard;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,94 +1,151 @@
-/** @format */
+'use client';
 
-"use client";
 import { useCallback, useState } from "react";
-import { signOut } from "next-auth/react";
-
 import { AiOutlineMenu } from "react-icons/ai";
-
-import Avatar from "../Avatar";
-import MenuItem from "./MenuItem";
-
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import useRentModal from "@/app/hooks/useRentModal";
-
-import { SafeUser } from "@/app/types";
+import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useRentModal from "@/app/hooks/useRentModal";
+import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
+
 interface UserMenuProps {
-	currentUser?: SafeUser | null;
+  currentUser?: SafeUser | null
 }
-const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
-	const router = useRouter()
-	const [isOpen, setIsOpen] = useState(Boolean);
 
-	const loginModal = useLoginModal();
-	const registerModal = useRegisterModal();
-	const rentModal = useRentModal();
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
+  const router = useRouter();
 
-	const toggle1open = useCallback(() => {
-		setIsOpen((value) => !value);
-	}, []);
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const rentModal = useRentModal();
 
-	const onRent = useCallback(() => {
-		if (!currentUser) {
-			return loginModal.onOpen();
-		}
-		// Open Rent model
-		rentModal.onOpen()
-	}, [currentUser, loginModal,rentModal]);
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<div className='flex flex-row items-center gap-3'>
-			<div
-				onClick={onRent}
-				className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
-				Airbnb your home
-			</div>
-			<div
-				onClick={toggle1open}
-				className='p-4 md:py-1 md:px-1 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
-				<AiOutlineMenu />
-				<div className='hidden md:block'>
-					<Avatar src={currentUser?.image} />
-				</div>
-			</div>
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
 
-			{isOpen && (
-				<div
-					className=' absolute 
-                                rounded-xl 
-                                shadow-md
-                                w-[40vw]
-                                md:w-3/4 
-                                bg-white 
-                                overflow-hidden 
-                                right-0 
-                                top-12 
-                                text-sm'>
-					<div className='flex flex-col cursor-pointer'>
-						{currentUser ? (
-							<>
-								<MenuItem onClick={() => router.push("/trips")} label='My trips' />
-								<MenuItem onClick={() => router.push("/favorites")} label='My favorites' />
-								<MenuItem onClick={() => router.push("/reservations")} label='My reservations' />
-								<MenuItem onClick={() => router.push("/properties")} label='My properties' />
-								<MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
-								<hr />
-								<MenuItem onClick={() => signOut()} label='Logout' />
-							</>
-						) : (
-							<>
-								<MenuItem onClick={loginModal.onOpen} label='login' />
-								<MenuItem onClick={registerModal.onOpen} label='Sign up' />
-							</>
-						)}
-					</div>
-				</div>
-			)}
-		</div>
-	);
-};
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
 
+    rentModal.onOpen();
+  }, [loginModal, rentModal, currentUser]);
+
+  return ( 
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          onClick={onRent}
+          className="
+            hidden
+            md:block
+            text-sm 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-neutral-100 
+            transition 
+            cursor-pointer
+          "
+        >
+          Airbnb your home
+        </div>
+        <div 
+        onClick={toggleOpen}
+        className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+			z-50
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer ">
+            {currentUser ? (
+              <>
+                <MenuItem 
+                  label="My trips" 
+                  onClick={() => router.push('/trips')}
+                />
+                <MenuItem 
+                  label="My favorites" 
+                  onClick={() => router.push('/favorites')}
+                />
+                <MenuItem 
+                  label="My reservations" 
+                  onClick={() => router.push('/reservations')}
+                />
+                <MenuItem 
+                  label="My properties" 
+                  onClick={() => router.push('/properties')}
+                />
+                <MenuItem 
+                  label="Airbnb your home" 
+                  onClick={rentModal.onOpen}
+                />
+                <hr />
+                <MenuItem 
+                  label="Logout" 
+                  onClick={() => signOut()}
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem 
+                  label="Login" 
+                  onClick={loginModal.onOpen}
+                />
+                <MenuItem 
+                  label="Sign up" 
+                  onClick={registerModal.onOpen}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+   );
+}
+ 
 export default UserMenu;

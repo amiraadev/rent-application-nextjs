d30c76f13f15ef7c617feacc61a0118f0ef6b2ff
amiraallagui <allagui_fawzi@outlook.fr>
2024-01-28 18:23:38 +0100

    fixing the build bug locally

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,36 @@
+/** @format */
+
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
 
-import getListings, { 
-  IListingsParams
-} from "@/app/actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
-  searchParams: IListingsParams
-};
+	searchParams: IListingsParams;
+}
 
 const Home = async ({ searchParams }: HomeProps) => {
-	
-
 	const currentUser = await getCurrentUser();
-	let listings = await getListings({});
-	if(Object.keys({ searchParams }).length !== 0){
-		listings = await getListings(searchParams);
-	  }
+	const listings = await getListings(searchParams);
 
-//   const listings = await getListings({});
-  console.log(listings);
-  
+	console.log(listings);
 
-  if (listings.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
-  }
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState showReset />
+			</ClientOnly>
+		);
+	}
 
-  return (
-    <ClientOnly>
-      <Container>
-        <div 
-          className="
+	return (
+		<ClientOnly>
+			<Container>
+				<div
+					className='
             pt-24
             grid 
             grid-cols-1 
@@ -47,19 +40,18 @@ const Home = async ({ searchParams }: HomeProps) => {
             xl:grid-cols-5
             2xl:grid-cols-6
             gap-8
-          "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
-  )
-}
+          '>
+					{listings.map((listing: any) => (
+						<ListingCard
+							currentUser={currentUser}
+							key={listing.id}
+							data={listing}
+						/>
+					))}
+				</div>
+			</Container>
+		</ClientOnly>
+	);
+};
 
 export default Home;
